624c8f6a0ebc6a4266fa6fc9cf4100217b58c826
KEdupuganti01 <Kedupuganti01@indianatech.net>
2024-11-11 17:33:27 -0600

    test

--- src/pages/wallet/Wallet.js
+++ src/pages/wallet/Wallet.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Locations() {
   return (
     <div className='text-white flex justify-center items-center w-full h-screen'>
-        <h1 className='text-4xl'>Wallet page holder</h1>
+        <h1 className='text-4xl'>Wallet krishna</h1>
     </div>
   )
 }
