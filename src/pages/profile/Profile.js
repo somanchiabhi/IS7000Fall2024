3812b5bbeff08ae357cec61a1148b32c751ba72b
avulaajaykumarreddy <ajayavula682@gmail.com>
2024-11-12 14:07:56 -0500

    git push scrum 5

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Profile() {
   return (
     <div className='text-white flex justify-center items-center w-full h-screen'>
-        <h1 className='text-4xl'>Profile holder page</h1>
+        <h1 className='text-4xl'>Profile holder </h1>
     </div>
   )
 }
