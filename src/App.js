2eb121ae8d8c6b2a6ad227c1bfc6677ca3a57b75
avulaajaykumarreddy <ajayavula682@gmail.com>
2024-11-13 18:31:09 -0500

    added code to subscriptionadmin page scum-5 story

--- src/App.js
+++ src/App.js
@@ -8,15 +8,19 @@ import Subscription from './pages/subscription/Subscription';
 import Wallet from './pages/wallet/Wallet';
 import User from './pages/user/User';
 import Profile from './pages/profile/Profile';
+import SubscriptionAdmin from './pages/admin/batch/SubscriptionAdmin';
 
 function App() {
   return (
-    <div className="App bg-rose-400	  w-full h-screen" >
+    <div className="App bg-white	  w-full h-screen" >
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/markets' element={<MarketOverview />} />
         <Route path='/subscription' element={<Subscription />} />
+        <Route path='/subscriptionadmin' element={<SubscriptionAdmin />} />
+
+        
         <Route path='/wallet' element={<Wallet />} />
         <Route path='/user' element={<User />} />
         <Route path='/profile' element={<Profile />} />
